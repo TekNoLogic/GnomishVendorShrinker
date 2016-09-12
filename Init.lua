36c5228bac34b31287f4eea1f9693093828084f8
tekkub ʕ ´ᴥ` ʔ <git@tekkub.net>
2016-09-11 22:01:26 -0600

    Shuffle codes about

--- Init.lua
+++ Init.lua
@@ -16,32 +16,31 @@ end
 
 
 function ns.OnLoad()
-	Hide(MerchantNextPageButton)
-	Hide(MerchantPrevPageButton)
-	Hide(MerchantPageText)
-
-
 	local GVS = ns.NewMainFrame()
 	GVS:SetWidth(315)
 	GVS:SetHeight(294)
 	GVS:SetPoint("TOPLEFT", MerchantFrame, 8, -67)
 	GVS:Hide()
 
-
 	-- Reanchor the buyback button, it acts weird when switching tabs otherwise...
 	MerchantBuyBackItem:ClearAllPoints()
 	MerchantBuyBackItem:SetPoint("BOTTOMRIGHT", -7, 33)
 
-
+	-- The little class select dropdown show trigget a refresh
 	hooksecurefunc("SetMerchantFilter", function() GVS:GetScript("OnShow")(GVS) end)
 
-
+	-- Force show when we're loaded on demand and the tab is already selected
 	if MerchantFrame:IsVisible() and MerchantFrame.selectedTab == 1 then
 		GVS:Show()
 	end
 
 	-- Reparent the first 10 MerchantItem frames, so they only appear for buyback
 	for i=1,10 do _G["MerchantItem"..i]:SetParent(MerchantItem11) end
+
+	-- Hide frames we don't need now
+	Hide(MerchantNextPageButton)
+	Hide(MerchantPrevPageButton)
+	Hide(MerchantPageText)
 
 	-- Clean up our frame factories
 	for i,v in pairs(ns) do if i:match("^New") then ns[i] = nil end end
